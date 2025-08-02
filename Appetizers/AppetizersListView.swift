114ca514e0da1a0e26d86ed8f54c5e961e726743
Luan Belem Rivello <luanrivello@hotmail.com>
2025-08-02 18:25:34 -0300

    Adjusts app to use the NetworkManager

--- Appetizers/AppetizersListView.swift
+++ Appetizers/AppetizersListView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct AppetizersListView: View {
+    @State var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView(){
-            List (MockData.appetizers, id: \.id) { appetizer in
+            List (appetizers, id: \.id) { appetizer in
                 AppetizerCell(appetizer: appetizer)
             }
             .navigationTitle("Appetizers")
         }
+        .onAppear(perform: getAppetizers)
+    }
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers(completed: { result in
+            switch result {
+            case .success(let appetizers):
+                self.appetizers = appetizers
+            case .failure(let error):
+                print("AppetizersListView: \(error.localizedDescription)")
+            }
+        })
+    }
+}
+
+struct NoConnectionView: View {
+    var body: some View {
+        VStack(spacing: 50) {
+            Image(systemName: "globe")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.accentColor)
+                .padding(100)
+            Text("Loading...").bold().font(.title)
+        }.padding(.bottom, 50)
     }
 }
 
@@ -30,10 +57,6 @@ struct AppetizerCell: View {
             VStack (alignment: .leading, spacing: 5){
                 Text(appetizer.name).bold()
                 Text("R$ \(appetizer.price, specifier: "%.2f")")
-                Text("\(appetizer.id)")
-                    .font(.caption2)
-                    .lineLimit(1)
-                    .truncationMode(.middle)
             }.padding()
         }
     }
