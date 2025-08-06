017dd23c9a0abaf8b8dda7a319cdad451fda7287
Luan Belem Rivello <luanrivello@hotmail.com>
2025-08-06 08:31:24 -0300

    Adds icon

---
+++ Appetizers/AppetizerListViem/AppetizersDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizersDetailsView.swift
+//  Appetizers
+//
+//  Created by lbr on 06/08/25.
+//
+
+import SwiftUI
+
+struct AppetizersDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppetizersDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizersDetailsView()
+    }
+}
