55c3495b162b0627e30dafae11128e6651343817
Luan Belem Rivello <luanrivello@hotmail.com>
2025-08-11 17:17:29 -0300

    Fixes image not updating bug

--- Appetizers/Utilities/NetworkModels/RemoteImage.swift
+++ Appetizers/Utilities/NetworkModels/RemoteImage.swift
@@ -8,33 +8,41 @@
 import SwiftUI
 
 final class ImageLoader: ObservableObject {
-    
     @Published var image: Image? = nil
     
     func load(fromURLString urlString: String) {
-        NetworkManager.shared.downloadImage(fromURLString: urlString) { loadedImage in
-            guard let loadedImage = loadedImage else { return }
-            DispatchQueue.main.async {
-                self.image = Image(uiImage: loadedImage)
+            loggerRemoteImage.info("\(NSString(string: urlString)) request to download image")
+            NetworkManager.shared.downloadImage(fromURLString: urlString) { loadedImage in
+                
+                guard let loadedImage = loadedImage else {
+                    loggerRemoteImage.error("Missing image")
+                    return
+                }
+                
+                loggerRemoteImage.info("\(NSString(string: urlString)) Image loaded")
+                
+                DispatchQueue.main.async {
+                    self.image = Image(uiImage: loadedImage)
+                }
             }
-        }
     }
 }
 
 struct RemoteImage: View {
-    var image: Image?
-    
-    var body: some View {
-        image?.resizable() ?? Image("food-placeholder").resizable()
-    }
-}
-
-struct AppetizerRemoteImage: View {
     @StateObject var imageLoader = ImageLoader()
     let urlString: String
     
     var body: some View {
-        RemoteImage(image: imageLoader.image)
-            .onAppear { imageLoader.load(fromURLString: urlString) }
+        ( imageLoader.image ?? Image("food-placeholder") )
+        .resizable()
+        .onAppear { imageLoader.load(fromURLString: urlString) }
     }
 }
+
+struct RemoteImage_Previews: PreviewProvider {
+    static var previews: some View {
+        RemoteImage(urlString: "http://localhost:8080/appetizers/images/fried-cheese-ravioli.jpeg")
+            .scaledToFit()
+    }
+}
+
